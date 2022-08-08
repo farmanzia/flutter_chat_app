03b8d045be738bce07ab00580694982ea2530ae1
Muhammad Farman <farmanzia96@gmail.com>
2022-08-08 23:10:20 +0500

    fully athenthication and store data on firestore

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,3 +1,5 @@
+import 'package:chatapp_firebase/screen/login.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -6,8 +8,18 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(automaticallyImplyLeading: false, actions: [
+          InkWell(
+            onTap: () async {
+              await FirebaseAuth.instance
+                ..signOut().then((value) => Navigator.push(
+                    context, MaterialPageRoute(builder: (_) => LogInScreen())));
+            },
+            child: Icon(Icons.logout),
+          )
+        ]),
         body: Center(
-      child: Text("Home "),
-    ));
+          child: Text("Home "),
+        ));
   }
 }
